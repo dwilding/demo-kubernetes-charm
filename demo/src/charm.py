529ef91fe2bdec194d99b6c652e081691db7d833
Dave Wilding <tech@dpw.me>
2025-01-09 18:21:46 +0800

    Added draft templates with split logic

--- demo/src/charm.py
+++ demo/src/charm.py
@@ -8,31 +8,31 @@ import logging
 
 import ops
 
-from services import DemoServer
+from services import DemoServices
 
 logger = logging.getLogger(__name__)
 
 
-class DemoServerCharm(ops.CharmBase):
+class DemoCharm(ops.CharmBase):
     """Charm the application."""
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on["demo_container"].pebble_ready, self._on_pebble_ready)
 
-        # Use self._pebble to interact with Pebble, the container's service manager
+        # Use self._pebble to interact with Pebble, the service manager in the workload container
         self._pebble = self.unit.get_container("demo-container")
 
-        # Use self._services to TODO
-        self._services = DemoServer()
+        # Use self._services to specify the services and communicate with running services
+        self._services = DemoServices()
 
     def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Handle pebble-ready event."""
 
-        self._pebble.add_layer("demo-server", self._services.pebble_layer, combine=True)
+        self._pebble.add_layer("base-layer", self._services.pebble_layer, combine=True)
         self._pebble.replan()
 
         self.unit.status = ops.ActiveStatus()
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(DemoServerCharm)  # type: ignore
+    ops.main(DemoCharm)  # type: ignore
---
+++ demo/src/services.py
@@ -0,0 +1,37 @@
+"""Represents the services in the workload container.
+The services are managed by Pebble."""
+
+import logging
+
+from ops import pebble
+
+logger = logging.getLogger(__name__)
+
+
+class DemoServices:
+    def __init__(self):
+        pass
+        # TODO: What (if anything) would we typically do here?
+
+    @property
+    def pebble_layer(self) -> pebble.Layer:
+        layer: pebble.LayerDict = {
+            "summary": "Services in the workload container",
+            "description": "Pebble layer that specifies the services"
+            "services": {
+                "demo-service": {
+                    "override": "replace",
+                    "summary": "A service in the workload container",
+                    "command": "run-demo-service",
+                    "startup": "enabled"
+                }
+            }
+        }
+        return pebble.layer(layer)
+
+    # TODO: What else might this class typically provide?
+    #       We might contact the service (via localhost) to get its version/status
+    #
+    #       BUT, suppose we want to push a new config file for a service...
+    #       Can we use pebble.push() from within this module?
+    #       Or would we need to use self._pebble.push() in charm.py
