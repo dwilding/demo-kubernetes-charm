cf15053a65b35d98c1a73a813f1b9aa4610e3080
Dave Wilding <tech@dpw.me>
2025-01-23 21:24:57 +0800

    Refactored code after discussion within Canonical Charm Tech team

--- zinc-k8s-operator/src/charm.py
+++ zinc-k8s-operator/src/charm.py
@@ -8,24 +8,15 @@ import zinc
 
 
 logger = logging.getLogger(__name__)
+
 # TODO: Add logging statements
+# TODO: Add tests
 
 
 class ZincCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        framework.observe(
-            self.on.collect_unit_status,
-            self._on_collect_unit_status
-        )
-        framework.observe(
-            self.on["zinc"].pebble_ready,
-            self._on_zinc_pebble_ready
-        )
-        framework.observe(
-            self.on.get_admin_password_action,
-            self._on_get_admin_password
-        )
+        self._observe_events(framework)
         self._pebble = self.unit.get_container("zinc") # For managing the workload container
         self._zinc_config = {
             "port": 4080,
@@ -34,11 +25,58 @@ class ZincCharm(ops.CharmBase):
         }
         self._zinc = zinc.ZincAPI(self._zinc_config["port"]) # For interacting with Zinc over HTTP
 
+    def _observe_events(self, framework: ops.Framework):
+        framework.observe(self.on.collect_unit_status, self._on_collect_unit_status)
+        framework.observe(self.on["zinc"].pebble_ready, self._on_zinc_pebble_ready)
+        framework.observe(self.on.get_admin_password_action, self._on_get_admin_password)
+
+    def _on_collect_unit_status(self, event: ops.CollectStatusEvent):
+        """Report the status of the unit."""
+        event.add_status(self._get_unit_status_for_container())
+        # For a more complex charm, there might be other aspects to check.
+
+    def _get_unit_status_for_container(self) -> ops.StatusBase:
+        """Determine the status of the workload container and return an appropriate unit status."""
+        try:
+            service_info = self._pebble.get_service("zinc")
+        except (ops.pebble.APIError, ops.ModelError):
+            return ops.MaintenanceStatus("waiting for Pebble to be ready")
+        if not service_info.is_running():
+            return ops.MaintenanceStatus("waiting for Zinc to start")
+        # Zinc didn't exit on startup, but it might not be ready yet, so check its health.
+        checks = self._pebble.get_checks(level=ops.pebble.CheckLevel.READY)
+        for check_info in checks.values():
+            if check_info.status == ops.pebble.CheckStatus.DOWN:
+                return ops.MaintenanceStatus("waiting for Zinc to be ready")
+                # TODO: Should we return BlockedStatus instead?
+                #       To inspect failing checks, the admin could run:
+                #       ```
+                #       juju exec --unit <unit> -- PEBBLE_SOCKET=/charm/containers/zinc/pebble.socket \
+                #       /charm/bin/pebble checks --level=ready
+                #       ```
+                #       The command needs to be like this because the OCI image for Zinc has
+                #       no shell binaries (so juju can't ssh/exec in the workload container).
+        return ops.ActiveStatus()
+
+    def _on_zinc_pebble_ready(self, event: ops.PebbleReadyEvent):
+        """Use Pebble to start Zinc."""
+        self.unit.status = ops.MaintenanceStatus("starting Zinc")
+        if not self._update_zinc_config_from_peer_relation():
+            if self.unit.is_leader():
+                self._update_peer_relation_from_zinc_config()
+            else:
+                event.defer() # Wait for the leader to put Zinc config in the peer relation
+                return
+        self._add_pebble_layer()
+        self._pebble.replan()
+        version = self._zinc.get_version() # Retries with delay (in case of slow startup)
+        self.unit.set_workload_version(version)
+
     def _add_pebble_layer(self):
-        # The OCI image for Zinc only has two binaries: zincsearch and go-runner
-        # We'll use go-runner to achieve the equivalent of `bash -c '/bin/zincsearch | tee PATH'`
+        # The OCI image for Zinc only has two binaries: zincsearch and go-runner.
+        # We use go-runner to achieve the equivalent of `bash -c '/bin/zincsearch | tee PATH'`.
         command = "/bin/go-runner --log-file=/var/lib/zincsearch/zinc.log --also-stdout=true --redirect-stderr=true /bin/zincsearch"
-        # Define a pebble layer and add it to the workload container
+        # Define a pebble layer and add it to the workload container.
         layer: ops.pebble.LayerDict = {
             "summary": "Zinc service",
             "description": "Pebble layer that specifies how to run Zinc",
@@ -55,38 +93,20 @@ class ZincCharm(ops.CharmBase):
                     },
                 },
             },
+            "checks": {
+                "zinc-ready": {
+                    "override": "replace",
+                    "level": "ready",
+                    "period": "30s",
+                    "timeout": "5s",
+                    "threshold": 1,
+                    "http": {
+                        "url": f"{self._zinc.base_url}/version",
+                    },
+                }
+            },
         }
         self._pebble.add_layer("zinc", layer, combine=True)
-
-    def _on_collect_unit_status(self, event: ops.CollectStatusEvent):
-        try:
-            service_info = self._pebble.get_service("zinc")
-        except (ops.pebble.APIError, ops.ModelError):
-            event.add_status(
-                ops.MaintenanceStatus("waiting for Pebble to be ready")
-            )
-            return
-        if not service_info.is_running():
-            event.add_status(
-                ops.MaintenanceStatus("waiting for Zinc to be ready")
-            )
-            return
-        # Zinc didn't exit on startup, but it might not be ready yet, so probe its API
-        version = self._zinc.get_version() # Includes retry logic in case of slow startup
-        self.unit.set_workload_version(version)
-        event.add_status(ops.ActiveStatus()) # Zinc is ready
-
-    def _on_zinc_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Use Pebble to start Zinc."""
-        self.unit.status = ops.MaintenanceStatus("waiting for Zinc to be ready")
-        if not self._update_zinc_config_from_peer_relation():
-            if self.unit.is_leader():
-                self._update_peer_relation_from_zinc_config()
-            else:
-                event.defer() # Wait for the leader to put Zinc config in the peer relation
-                return
-        self._add_pebble_layer()
-        self._pebble.replan()
 
     def _on_get_admin_password(self, event: ops.ActionEvent):
         """Return the initial admin password as an action response."""
--- zinc-k8s-operator/src/zinc.py
+++ zinc-k8s-operator/src/zinc.py
@@ -5,25 +5,31 @@ import requests
 
 
 logger = logging.getLogger(__name__)
+
 # TODO: Add logging statements
+# TODO: Add tests
 
 
 class ZincAPI:
     """Client for interacting with Zinc over HTTP."""
     def __init__(self, port: int):
         self.port = port
-        # Define a requests session that retries failed connections
-        self._requests = requests.Session()
+        self.base_url = f"http://localhost:{self.port}"
+        # Define a requests session that retries failed connections.
+        self._session_with_retry = requests.Session()
         adapter = requests.adapters.HTTPAdapter(
             max_retries=requests.packages.urllib3.util.retry.Retry(
                 total=4,
                 backoff_factor=1
             ) # Retry after 0, 2, 4, 8 seconds
         )
-        self._requests.mount("http://", adapter)
+        self._session_with_retry.mount("http://", adapter)
 
-    def get_version(self) -> str:
-        response = self._requests.get(f"http://localhost:{self.port}/version", timeout=5)
+    def get_version(self, session: requests.Session | None = None) -> str:
+        response = self._session_with_retry.get(
+            f"{self.base_url}/version",
+            timeout=5
+        )
         response_dict = response.json()
-        version = response_dict["version"].strip("'") # Version is double quoted for some reason
+        version = response_dict["version"].strip("'") # Version is double quoted
         return version
