da8ec3830533ca72a29430a06fe3e544515c4838
Dave Wilding <tech@dpw.me>
2025-01-24 08:15:02 +0800

    Removed code (will re-add it in PRs for easier review)

--- zinc-k8s-operator/pebble.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-UNIT="$1"
-shift
-# Run Pebble in the charm container of the unit, connected to the workload container via the shared socket
-juju exec --unit="zinc-k8s/$UNIT" -- PEBBLE_SOCKET=/charm/containers/zinc/pebble.socket /charm/bin/pebble "$@"
